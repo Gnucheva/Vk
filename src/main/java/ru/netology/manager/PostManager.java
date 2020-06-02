b1876d5d749b059b3576dc959eb582ec116e9493
Anastasia <n41111215@mail.ru>
2020-06-02 23:56:49 +0300

    Add manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int owner_id, String domain, String query, int owners_only, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
