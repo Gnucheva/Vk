6ff689c532ac2b78427801e3060fc78bdb310005
Anastasia <n41111215@mail.ru>
2020-06-08 18:49:38 +0300

    edit

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,7 +5,7 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] search(int owner_id, String domain, String query, int owners_only, int count, int offset) {
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
         return null;
     }
 
